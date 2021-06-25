ccb09ccd8d0094e83a8389581f2543f8ec5214dc
Jere265 <66541165+Jere265@users.noreply.github.com>
2021-06-25 15:35:55 -0300

    script para colocar las fichas hecho

--- script.js
+++ script.js
@@ -10,17 +10,20 @@ empezarElJuego()
 function empezarElJuego() {
     turnoDelRojo = true
     celdas.forEach(celda => {
-        celda.addEventListener('click', click, { once: true })
+        celda.addEventListener('click', click/*, { once: true }*/)
     })
     activarHover()
 }
 
 function click(e) {
     const celda = e.target
+    /*console.log(celda.innerText)*/
     const claseActual = turnoDelRojo ? CLASE_ROJO : CLASE_AZUL
-    ponerFicha(celda, claseActual)
-    cambiarTurno()
-    activarHover()
+    if(sePuedePonerFicha(celda)) {
+        ponerFicha(celda, claseActual)
+        cambiarTurno()
+        activarHover()
+    }  
 }
 
 function ponerFicha(celda,claseActual) {
@@ -41,3 +44,22 @@ function activarHover() {
     }
 }
 
+function sePuedePonerFicha(celda) {
+    console.log(celda.innerText, "celda seleccionada")
+    var celdaDeAbajo = parseInt(celda.innerText) + 7
+    console.log(celdaDeAbajo, "celda de abajo")
+
+    if(celdas[celda.innerText].classList.contains(CLASE_ROJO) || celdas[celda.innerText].classList.contains(CLASE_AZUL)) {
+        return false
+    }
+
+    if(celda.innerText == 35 || celda.innerText == 36 || celda.innerText == 37 || celda.innerText == 38 || celda.innerText == 39 || celda.innerText == 40 || celda.innerText == 41) {
+        return true
+    } else if (celdas[celdaDeAbajo].classList.contains(CLASE_ROJO) || celdas[celdaDeAbajo].classList.contains(CLASE_AZUL)) {
+        sePuede = true
+        return sePuede
+    } else {
+        return false
+    }
+    
+}
