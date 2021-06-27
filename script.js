5b3da695bf25093e5abcfe631f8f64560ebdceeb
Jere265 <66541165+Jere265@users.noreply.github.com>
2021-06-27 05:49:05 -0300

    empezado el script para saber quien gana

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ let turnoDelRojo
 const CLASE_ROJO = 'ficharoja'
 const CLASE_AZUL = 'fichaazul'
 
-
 empezarElJuego()
 
 function empezarElJuego() {
@@ -17,16 +16,16 @@ function empezarElJuego() {
 
 function click(e) {
     const celda = e.target
-    /*console.log(celda.innerText)*/
     const claseActual = turnoDelRojo ? CLASE_ROJO : CLASE_AZUL
     if(sePuedePonerFicha(celda)) {
         ponerFicha(celda, claseActual)
+        chequearSiGano(claseActual)
         cambiarTurno()
         activarHover()
     }  
 }
 
-function ponerFicha(celda,claseActual) {
+function ponerFicha(celda, claseActual) {
     celda.classList.add(claseActual)
 }
 
@@ -47,7 +46,7 @@ function activarHover() {
 function sePuedePonerFicha(celda) {
     console.log(celda.innerText, "celda seleccionada")
     var celdaDeAbajo = parseInt(celda.innerText) + 7
-    console.log(celdaDeAbajo, "celda de abajo")
+    //console.log(celdaDeAbajo, "celda de abajo")
 
     if(celdas[celda.innerText].classList.contains(CLASE_ROJO) || celdas[celda.innerText].classList.contains(CLASE_AZUL)) {
         return false
@@ -60,6 +59,41 @@ function sePuedePonerFicha(celda) {
         return sePuede
     } else {
         return false
+    }   
+}
+
+function chequearSiGano(claseActual) {
+    /*let arrayBi = [6][7]
+    for(i = 0; i < celdas.length; i++) {
+        for(j = 0; j < 6; j++) {
+            for(k = 0; k < 7; k++) {
+                arrayBi[j][k] = celdas[i].classList.contains(claseActual)
+            }
+        }
+    }*/
+    var fila1 = []
+    var fila2 = []
+    var fila3 = []
+    var fila4 = []
+    var fila5 = []
+    var fila6 = []
+
+    for(i = 0; i < 7; i++) {
+                fila1.push(celdas[i].classList.contains(claseActual))
+                fila2.push(celdas[i+7].classList.contains(claseActual))
+                fila3.push(celdas[i+14].classList.contains(claseActual))
+                fila4.push(celdas[i+21].classList.contains(claseActual))
+                fila5.push(celdas[i+28].classList.contains(claseActual))
+                fila6.push(celdas[i+35].classList.contains(claseActual))
+                //console.log(fila1[i],"fila1[i]")
+                //console.log(celdas[0].classList.contains(claseActual), "celdas[0]")
     }
+    for(i = 0; i < 7; i++) {
+        //console.log(fila1[i],"fila1[i]:",i)
+        console.log(fila6[i],"fila6[i]:",i)
+    }
+    //var fila1 = celdas.classList.contains(claseActual).slice(0,7)
+    //console.log(fila1)
     
+
 }
